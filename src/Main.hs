e92e713b191871fa46830c75b5553f55015fb917
Tobias Reinhart <TobiasReinhart@faustud-010-021-006-182.pool.uni-erlangen.de>
2019-01-16 14:48:16 +0100

    filter new

--- src/Main.hs
+++ src/Main.hs
@@ -544,7 +544,7 @@ module Main (
 
         let ansatzTopSortnoFilter = getAllIndsLabelInverse "abcdmnefghpqijklrs" [(1,2),(1,3),(3,4),(3,5),(5,6),(5,7),(7,8),(7,9),(9,10),(9,11),(11,12),(11,13),(13,14),(13,15),(15,16),(15,17),(17,18)] [] []
 
-        writeFile "/cip/austausch/cgg/topSorts18Filtered.txt" ansatzTopSort
+        writeFile "/cip/austausch/cgg/topSorts18Filtered2.txt" ansatzTopSort
 
 
 

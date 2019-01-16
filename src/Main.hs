67da057e94f424d71a6856530af6d774ce917189
Tobias Reinhart <TobiasReinhart@faustud-010-021-006-182.pool.uni-erlangen.de>
2019-01-16 13:01:48 +0100

    topsorts added

--- src/Main.hs
+++ src/Main.hs
@@ -534,13 +534,15 @@ module Main (
 
         let ansatzCondAI = mkEqnSparseAnsatzAI $ ansatzAI3 map1Area map2Area map1Metric map2Metric
 
-        writeFile "/cip/austausch/cgg/Ansatz/ansatzA_16_1_19.txt" $ showEqnsFlatMatLab ansatzCondA
+        --writeFile "/cip/austausch/cgg/Ansatz/ansatzA_16_1_19.txt" $ showEqnsFlatMatLab ansatzCondA
 
-        writeFile "/cip/austausch/cgg/Ansatz/ansatzAa_16_1_19.txt" $ showEqnsFlatMatLab ansatzCondAa
+        --writeFile "/cip/austausch/cgg/Ansatz/ansatzAa_16_1_19.txt" $ showEqnsFlatMatLab ansatzCondAa
 
-        writeFile "/cip/austausch/cgg/Ansatz/ansatzAI_16_1_19.txt" $ showEqnsFlatMatLab ansatzCondAI
+        --writeFile "/cip/austausch/cgg/Ansatz/ansatzAI_16_1_19.txt" $ showEqnsFlatMatLab ansatzCondAI
 
+        let ansatzTopSort = getAllInds [(1,3),(1,2),(3,4),(5,7),(5,6),(7,8),(9,11),(9,10),(11,12),(13,14),(15,16),(1,5),(5,9)] [] [(1,3),(3,5),(5,7),(7,9),(9,11),(11,13),(13,15),(1,2),(3,4),(5,6),(7,8),(9,10),(11,12),(13,14),(15,16)]
 
+        writeFile "/cip/austausch/cgg/topSorts16.txt" $ show ansatzTopSort
 
 
 

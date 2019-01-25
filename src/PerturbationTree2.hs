55f51e4bbcb52fd5a4f4d745b732edd2e34f27c4
Tobias Reinhart <TobiasReinhart@faustud-010-021-026-181.pool.uni-erlangen.de>
2019-01-25 10:15:38 +0100

    cabal changed

--- src/PerturbationTree2.hs
+++ src/PerturbationTree2.hs
@@ -7,12 +7,11 @@ module PerturbationTree2 (
 
 ) where
 
-    import Data.Tree
     import qualified Data.IntMap.Strict as I
+    import qualified Data.Map.Strict as M
     import Data.Foldable
     import Data.List 
-
-  
+    import Data.Maybe
 
     getAllIndsEta :: [Int] -> [[Int]]
     getAllIndsEta [a,b] = [[a,b]]
@@ -31,3 +30,102 @@ module PerturbationTree2 (
                 s = length l
                 l2 = getIndsEpsilon s
                 l3 = concat $ map (\x -> (++) x <$> (getAllIndsEta (foldr delete l x))) l2
+
+    filter1Sym :: [Int] -> (Int,Int) -> Bool 
+    filter1Sym l (i,j)   
+            | first == i = True
+            | otherwise = False  
+             where
+               first = fromJust $ find (\x -> x == i || x == j) l
+
+    filterSym :: [Int] -> [(Int,Int)] -> Bool
+    filterSym l inds = and boolList 
+            where
+               boolList = map (filter1Sym l) inds 
+
+    getEtaInds :: [Int] -> [(Int,Int)] -> [[Int]]
+    getEtaInds l sym = filter (\x -> filterSym x sym) $ getAllIndsEta l
+
+    getEpsilonInds :: [Int] -> [(Int,Int)] -> [[Int]]
+    getEpsilonInds l sym = filter (\x -> filterSym x sym) $ getAllIndsEpsilon l
+
+    type Eta = (Int,Int)
+
+    type Epsilon = (Int,Int,Int,Int)
+
+    type Var = (Rational,Int) 
+
+    sortEta :: Eta -> Eta 
+    sortEta (i,j) = (min i j, max i j)
+
+    sortEpsilon :: Epsilon -> Epsilon
+    sortEpsilon (i,j,k,l) = (a,b,c,d)
+        where
+            [a,b,c,d] = sort [i,j,k,l]
+
+    signEpsilon :: Epsilon -> Int
+    signEpsilon (i,j,k,l) = (-1)^(length $  filter (==True) [j>i,k>i,l>i,k>j,l>j,l>k])
+
+    data AnsatzNode = EtaNode Eta | EpsilonNode Epsilon deriving (Show, Eq)
+
+    instance Ord AnsatzNode where
+        compare (EtaNode eta1) (EtaNode eta2) = compare eta1 eta2
+        compare (EpsilonNode eps1) (EpsilonNode eps2) = compare eps1 eps2
+        compare (EtaNode _) (EpsilonNode _) =  GT
+
+    data AnsatzForest = Leaf Var | Forest (M.Map AnsatzNode AnsatzForest) | EmptyForest deriving (Show, Eq)
+
+    isLeaf :: AnsatzForest -> Bool
+    isLeaf (Leaf var) = True
+    isLeaf forest = False
+
+    mapNodes :: (AnsatzNode -> AnsatzNode) -> AnsatzForest -> AnsatzForest
+    mapNodes f (Leaf var) = Leaf var
+    mapNodes f (Forest m) = Forest $ (M.mapKeys f).(M.map (mapNodes f)) $ m
+
+    mapVars :: (Var -> Var) -> AnsatzForest -> AnsatzForest 
+    mapVars f (Leaf var) = Leaf $ f var
+    mapVars f ans = ans
+
+    --add 2 sorted forests
+
+    addForests :: AnsatzForest -> AnsatzForest -> AnsatzForest
+    addForests (Leaf (fac1,lab1)) (Leaf (fac2,lab2))
+            | lab1 /= lab2 = error "should not be necessary to add Forests with different label"
+            | fac1 + fac2 == 0 = EmptyForest
+            | otherwise = Leaf (fac1+fac2,lab1)
+    addForests (Forest m1) (Forest m2) = Forest $ M.unionWith filteredSum m1 m2
+            where
+                filteredSum = \ a b -> rmZeros $ addForests a b 
+           
+
+    rmZeros :: AnsatzForest -> AnsatzForest
+    rmZeros (Leaf x) = Leaf x
+    remZeros (Forest m) = Forest $ M.filter (/= EmptyForest) m 
+
+    --for adding Forests must be sorted s.t. each Node at one Level is LT each Node at the Level below
+
+    isSortedNode :: AnsatzNode -> AnsatzForest -> Bool
+    isSortedNode node (Leaf var) = True
+    isSortedNode node (Forest m) 
+                | isLeaf (Forest m) = True
+                | otherwise = node < nextNode
+                where
+                    nextNode = head $ M.keys m
+    {-
+    sortForest :: AnsatzForest -> AnsatzForest
+    sortForest (Leaf var) = Leaf var
+    sortForest (Forest m) = M.foldrWithKey 
+            where
+                sortedSubForest = Forest $ (M.map sortForest) m 
+                foldF = \node 
+             
+    swap1Node :: (AnsatzNode,AnsatzForest) -> [(AnsatzNode,AnsatzForest)]
+    swap1Node (node,Forest m) = M.traverseWithKey (\k a -> M.singleton $ (a))
+    -}
+    
+    
+
+    
+
+

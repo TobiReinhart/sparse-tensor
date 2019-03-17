fa08fdc36b43a93b39f31a134ac603501f4ef636
Tobias Reinhart <TobiasReinhart@Tobiass-Air.fritz.box>
2019-03-17 10:25:50 +0100

    ansatz 18_3 corrected

--- src/TensorTreeNumeric4.hs
+++ src/TensorTreeNumeric4.hs
@@ -998,10 +998,10 @@ module TensorTreeNumeric4 (
                 aSym = tensorContr3 (1,1) $ tensorProd8 invEta antiSym
                 intAContr = tensorContr3 (0,0) $ tensorContr3 (0,1) $ tensorProd8 intA aSym
                 int2Contr = tensorContr3 (1,0) $ tensorContr3 (0,2) $ tensorProd8 int2 aSym
-                block1 = tensorContrWith20 (1,0) addVarsMap $ tensorProdWith8 multVarsMap intAContr ansatzTens
+                block1 = tensorTransWithU20 (1,3) $ tensorContrWith20 (0,0) addVarsMap $ tensorProdWith8 (flip multVarsMap) ansatzTens intAContr
                 block2 = tensorTransWithU20 (0,1) addVarsMap block1
                 block3 = tensorContrWith3 (0,0) addVarsMap $ tensorContrWith20 (2,0) addVarsMap $ tensorProdWith8 (flip multVarsMap) ansatzTens int2Contr 
-                block4 = tensorTransWithU3 (0,1) addVarsMap $ tensorTransWithU20 (2,3) addVarsMap block2 
+                block4 = tensorTransWithU3 (0,1) addVarsMap $ tensorTransWithU20 (2,3) addVarsMap block3 
 
 
 

b4525e1289ed1586e35400fc6e87bd79955d812a
Tobias Reinhart <TobiasReinhart@Tobiass-Air.fritz.box>
2019-04-10 18:17:59 +0200

    ansatzABCcDd

--- src/TensorTreeNumeric4_2.hs
+++ src/TensorTreeNumeric4_2.hs
@@ -35,7 +35,7 @@
     contrATens1, contrATens2, contrATens3,
     decodeTensor, encodeTensor, ansVarToAreaVar, 
     mapTo1, mapTo2, mapTo3, mapTo4, mapTo5, mapTo6,
-    resortTens1,
+    resortTens1, resortTens5,
     (&>), singletonTList, toEMatrix6 
  
     

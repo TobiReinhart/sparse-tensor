2227d4570a4a18ecd79497af97c715c49b1e3038
Tobias Reinhart <TobiasReinhart@faustud-010-021-026-160.pool.uni-erlangen.de>
2019-04-10 14:35:01 +0200

    ansatz 18 test

--- src/Main.hs
+++ src/Main.hs
@@ -52,27 +52,27 @@ main = do
 
     
 
-    --ansatz18' <- BS.readFile "/cip/austausch/cgg/7.4.tens18"
+    ansatz18' <- BS.readFile "/cip/austausch/cgg/7.4.tens18"
     
-    --let ansatz18 = decodeTensor ansatz18' :: ATens 3 0 3 0 0 0 AnsVar
+    let ansatz18 = decodeTensor ansatz18' :: ATens 3 0 3 0 0 0 AnsVar
 
-    ansatz18_2' <- BS.readFile "/cip/austausch/cgg/7.4.tens18_2" 
+    --ansatz18_2' <- BS.readFile "/cip/austausch/cgg/7.4.tens18_2" 
     
-    let ansatz18_2 = decodeTensor ansatz18_2' :: ATens 4 0 1 0 0 0 AnsVar
+    --let ansatz18_2 = decodeTensor ansatz18_2' :: ATens 4 0 1 0 0 0 AnsVar
 
     --ansatz18_3' <- BS.readFile "/cip/austausch/cgg/7.4.tens18_3" 
     
     --let ansatz18_3 = decodeTensor ansatz18_3' :: ATens 4 0 0 0 2 0 AnsVar
 
-    --let ans18 = ansatzAIBJCK ansatz18 
+    let ans18 = ansatzAIBJCK ansatz18 
 
-    let ans18_2 = ansatzABCDJ ansatz18_2 
+    --let ans18_2 = ansatzABCDJ ansatz18_2 
 
     --let ans18_3 = ansatzABCcDd ansatz18_3 
 
-    --print $ toListT6 ansatz18 
+    print $ toListT6 ansatz18 
     
-    print $ toListShowVar6 ans18_2 
+    --print $ toListShowVar6 ans18_2 
 
     --print $ toListShowVar6 ans18_3
 

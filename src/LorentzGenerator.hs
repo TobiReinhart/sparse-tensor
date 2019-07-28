8b3be8e01b7d98ae4f6cefac76c00d3a978c6385
Tobias Reinhart <TobiasReinhart@Tobiass-Air.fritz.box>
2019-07-28 15:31:57 +0200

    pre selection finished

--- src/LorentzGenerator.hs
+++ src/LorentzGenerator.hs
@@ -50,8 +50,12 @@ module LorentzGenerator (
 
     import SparseTensor
 
-    --the first step consist of pre-reducing the index list for the eta and epsilon trees as much as possible 
-    --this is done by using the symmetries 
+    {--
+    The first step consist of pre-reducing the index list for the eta and epsilon trees as much as possible.
+    This is done by using the symmetries inthe sense that we try to select exactly one representative out of each class of indices
+    that are equivalent under the symmetries. 
+    Note that the prereduction is not necessary but increases performance.
+    --}
 
     --symmetry type alias: (SymPairs, ASymPairs, BlockSyms, CyclicSyms, CyclicBlockSyms)
 
@@ -88,8 +92,12 @@ module LorentzGenerator (
                boolList = map (filter1Sym l) inds 
 
 
-    --note that writing specific indices from a block symmetry at an eta yields additional symmetries: for instance consider the block symmetry 
-    --[ab] <-> [cd] writing eta[ac] yields the new symmetry b <-> d. 
+    {--
+    Note that writing specific indices from a block symmetry at an eta yields additional symmetries: for instance consider the block symmetry 
+    [ab] <-> [cd] writing eta[ac] yields the new symmetry b <-> d. The 2-block symmetry is thus reduced to a 1-block symmetry. In the same way 
+    etas reduce n-block symmetries to (n-1)-block symmetries. To compute these we also need to include all possible block symmetries that are specified
+    in ters of a cyclic block symmetry.
+    --}
 
     getExtraSyms1 :: [Int] -> Symmetry -> Symmetry 
     getExtraSyms1 [] syms = ([],[],[],[],[]) 
@@ -107,30 +115,37 @@ module LorentzGenerator (
             where 
                 l = map (\y -> (x,y)) xs
 
+    {--
+    Furthermore distributing a symmetric or antisymmetric pair of indices over 2 etas yields an additinal symmetry or antisymmetry
+    of the remaining eta indices due to the product structure: for instance consider the a <-> b symmetry,
+    writing eta[ac] eta[bd] yields an additional c <-> d symmetry. Here it is additionally necessary to include the pair symmetries that are contributed by a given total symmetry 
+    --}
 
-    --furthermore distributing a symmetric or antisymmetric pair over 2 etas yields an additinal symmetry: for instance consider the < <-> b symmetry,
-    --writing eta[ac] eta[bd] yields an additional c <-> d symmetry.
+
+    --given one eta, if the eta contains an index from a symmetric or antisymmetric pair return the corresponding second index and the other index of the eta
 
     get2nd :: [Int] -> Symmetry -> (Maybe [(Int,Int)], Maybe [(Int,Int)]) 
     get2nd [a,b] (pairs,aPairs,blocks,cycles,blockCycles) = (sndPairs, sndAPairs)
             where 
                 allPairs = pairs ++ (concat $ map mkSymsFromCycle cycles)
-                aPair = lookup a pairs 
-                bPair = lookup b  (map swap pairs) 
+                aPair = lookup a allPairs 
+                bPair = lookup b  (map swap allPairs) 
                 aAPair = lookup a aPairs
                 bAPair = lookup b (map swap aPairs)
                 sndPairs = case (aPair, bPair) of 
                                 (Nothing, Nothing)  ->  Nothing 
                                 (Just x, Nothing)   -> Just [(b,x)] 
                                 (Nothing, Just y)   -> Just [(a,y)] 
-                                (Just x, Just y)    -> Just [(b,x),(a,y)]
+                                (Just x, Just y)    -> if x == b then Nothing else Just [(b,x),(a,y)]
                 sndAPairs = case (aAPair, bAPair) of 
                                 (Nothing, Nothing)  ->  Nothing 
                                 (Just x, Nothing)   -> Just [(b,x)] 
                                 (Nothing, Just y)   -> Just [(a,y)] 
-                                (Just x, Just y)    -> Just [(b,x),(a,y)]
+                                (Just x, Just y)    -> if x == b then error "eta indices are anti symmetric!" else  Just [(b,x),(a,y)]
 
 
+    --find the eta that contains the computed second pair index and return the other indices of this eta
+                
     get2ndSyms :: Maybe [(Int,Int)] -> Symmetry -> [[Int]] -> Symmetry 
     get2ndSyms Nothing syms etas = syms
     get2ndSyms (Just i) (pairs,aPairs,blocks,cycles,blockCycles) etas = (newPairs,[],[],[],[])  
@@ -152,6 +167,8 @@ module LorentzGenerator (
             get2ndInd l (i,j) = mapMaybe (\[a,b] -> if j == a then Just (i,b) else if j == b then Just (i,a) else Nothing) l
             newAPairs = concat $ map (get2ndInd etas) i 
 
+
+    --apply to whole ind list
 
     getExtraSyms2 :: [Int] -> Symmetry -> Symmetry 
     getExtraSyms2 [] syms = syms 
@@ -202,19 +219,20 @@ module LorentzGenerator (
                 filters1 = mkFilters (p,ap,b,c,bc) 
                 allInds = getAllIndsEta inds ap 
 
-    --now we proceed in the same fashion for the epsilon ind list
+    {--
+    Now we proceed in the same fashion for the epsilon ind list.
+    Here we can actually from the very begining prevent some linear dependencies from occuring by noting that due to certain symmetries 
+    certain expressions involving epsilon only differ by an expression that is antisymmetric in 5 or more indices and hence vanishes
+    we restrict to the simplest case: two antisymmetric pairs with a block symmetry, i.e. an area block
 
-    --here we can actually from the very beggining prevent some linear dependencies from occuring by noting that due to certain symmetries 
-    --certain expressions involving epsilon only differ by an expression that is antisymmetric in 5 or more indices and hence vanishes
-
-    --we restrict to the simplest case: two antisymmetric pairs with a block symmetry, i.e. an area block
-
-    {-we can use the following observations :
+    we can use the following observations :
         as we want to contstruct a basis it suffices to pick representatives of the different symmetry orbits module anti-sym in (>4) indices
             1) whenever 3 indices of one are metric are contracted against an epsilon we can actually express the tensor as one with 4 area indices contracted against epsilon
             2) all tensors with 2 area indices contracted against one epsilon can be expressed as tensors with the first 2 area indices contracted against epsilon 
             3) tensors with a maximum of 1 epsilon contraction per area metric can be exprerssed by those with at least one 2 area contraction 
-    -}
+    --}
+
+    --get all possible epsilon inds that are allowed under the above considerations 
 
     getAllIndsEpsilon :: [Int] -> Symmetry  -> [[Int]]
     getAllIndsEpsilon inds (p,ap,b,cyc,cb)  = [ [a,b,c,d] | a <- [1..i-3], b <- [a+1..i-2], c <- [b+1..i-1], d <- [c+1..i], 
@@ -248,10 +266,13 @@ module LorentzGenerator (
                             | otherwise = False 
                         is1Area list [i,j,k,l] = (maximum $ map (\x -> length $ intersect [i,j,k,l] x) list) == 1 
 
+    --a 2-block symmetry with the respectively first indices at an epsilon yields an additional antisymmetry (note that we did not include higher block antisymmmetries)
+
     getExtraASymsEps :: [Int] -> Symmetry -> Symmetry 
     getExtraASymsEps eps (p,ap,blo,cyc,cb) = ([],newASyms, [], [], [])
             where 
-                blocks2 = filter (\(a,b) -> length a == 2) blo 
+                allBlocks = blocks ++ (concat $ map mkBlocksFromBlockCycle blockCycles) 
+                blocks2 = filter (\(a,b) -> length a == 2) allBlocks
                 newASyms = mapMaybe (\([i,j],[k,l]) -> if (length $ intersect [i,k] eps) == 2 then Just (j,l) else if (length $ intersect [j,l] eps) == 2  then Just (i,k) else Nothing) blocks2
 
     getEpsilonInds :: [Int] -> Symmetry -> [[Int]]

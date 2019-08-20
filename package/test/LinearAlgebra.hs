a972c4a4acfe7d19de48e7bd4f238fd8b18bd077
Nils Alex <nils@famalex.de>
2019-08-20 22:50:05 +0200

    removed InfiniteList from test

--- package/test/LinearAlgebra.hs
+++ package/test/LinearAlgebra.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase #-}
 
 module LinearAlgebra (props_LinearAlgebra) where
 
 import Test.QuickCheck
+import Test.QuickCheck.Arbitrary
+import Test.QuickCheck.Gen
 import Test.QuickCheck.Modifiers
 
 import System.Exit
@@ -15,32 +18,46 @@ import Math.Tensor.Internal.LinearAlgebra (independentColumnsMat)
 
 data SmallInt = S0 | S1 deriving (Show, Ord, Eq, Enum, Bounded)
 
+toSmall :: Int -> SmallInt
+toSmall 0 = S0
+toSmall 1 = S1
+toSmall i = error $ "cannot convert " ++ show i ++ " to SmallInt"
+
 fromSmall :: Num a => SmallInt -> a
 fromSmall S0 = 0
 fromSmall S1 = 1
 
 instance Arbitrary SmallInt where
-    arbitrary = elements [S0, S1]
+    arbitrary = arbitraryBoundedEnum
 
-smallValues :: Positive Int -> Positive Int -> InfiniteList SmallInt -> Bool
-smallValues (Positive rows) (Positive cols) (InfiniteList xs _) =
+data MatrixData a = MatrixData (Positive Int) (Positive Int) [a] deriving Show
+
+instance Arbitrary a => Arbitrary (MatrixData a) where
+    arbitrary = do
+      m@(Positive m') <- arbitrary
+      n@(Positive n') <- arbitrary
+      xs <- vector (m'*n')
+      return $ MatrixData m n xs
+
+smallValues :: MatrixData SmallInt -> Bool
+smallValues (MatrixData (Positive rows) (Positive cols) xs) =
     rank mat' == rank mat
   where
-    mat  = (rows Matrix.>< cols) $ take (rows * cols) $ map fromSmall xs
+    mat  = (rows Matrix.>< cols) $ map fromSmall xs
     mat' = independentColumnsMat mat
 
-ints :: Positive Int -> Positive Int -> InfiniteList Int -> Bool
-ints (Positive rows) (Positive cols) (InfiniteList xs _) =
+ints :: MatrixData Int -> Bool
+ints (MatrixData (Positive rows) (Positive cols) xs) =
     rank mat' == rank mat
   where
-    mat  = (rows Matrix.>< cols) $ take (rows * cols) $ map fromIntegral xs
+    mat  = (rows Matrix.>< cols) $ map fromIntegral xs
     mat' = independentColumnsMat mat
 
-doubles :: Positive Int -> Positive Int -> InfiniteList Double -> Bool
-doubles (Positive rows) (Positive cols) (InfiniteList xs _) =
+doubles :: MatrixData Double -> Bool
+doubles (MatrixData (Positive rows) (Positive cols) xs) =
     rank mat' == rank mat
   where
-    mat  = (rows Matrix.>< cols) $ take (rows * cols) xs
+    mat  = (rows Matrix.>< cols) xs
     mat' = independentColumnsMat mat
 
 prop_linIndepColsSmall  = withMaxSuccess 1000 smallValues
